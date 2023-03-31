1092d71bef0168e585d55454c3d8d65b4b4a3b05
Down <not4profitnft@gmail.com>
2023-03-30 21:38:20 -0400

    fkin commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,21 +7,87 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "Not For Profitz";
+const description = "Down bad, but it's ok. I'm not in it for the money. I'm here for the community. WAGMI";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 10,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "1 of 1" },
+    ],
+  },{
+    growEditionSizeTo: 360,
+    layersOrder: [
+      { name: "A KID Called Beast" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 384,
+    layersOrder: [
+      { name: "Checks" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 1468,
+    layersOrder: [
+      { name: "Clone X" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 2811,
+    layersOrder: [
+      { name: "Cool Cats" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 3823,
+    layersOrder: [
+      { name: "Deadfellaz" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 4854,
+    layersOrder: [
+      { name: "Doodles" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 5739,
+    layersOrder: [
+      { name: "Goblintown" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 6266,
+    layersOrder: [
+      { name: "Gutter Cat Gang" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 6768,
+    layersOrder: [
+      { name: "Moonbirds" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 6819,
+    layersOrder: [
+      { name: "Mutant Hounds" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 6966,
+    layersOrder: [
+      { name: "Vee Friends" },
+      { name: "Stamp" },
+    ],
+  },{
+    growEditionSizeTo: 8000,
+    layersOrder: [
+      { name: "World Of Women" },
+      { name: "Stamp" },
     ],
   },
 ];
@@ -31,13 +97,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 500,
+  height: 500,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "notforprofitz.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -48,22 +114,22 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'goerli'; // only goerli, polygon, or ethereum
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'Not For Profitz';
+const CONTRACT_SYMBOL = 'NFP';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const OWNER_ADDRESS = '0xC4f67c3d87E55467CDc055986c61d0E9d9778365';
+const TREASURY_ADDRESS = '0xC4f67c3d87E55467CDc055986c61d0E9d9778365';
+const MAX_SUPPLY = 8000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 0.009; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 20; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2023-04-07T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0xC4f67c3d87E55467CDc055986c61d0E9d9778365"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
@@ -73,9 +139,9 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
-const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_TITLE = "Not for Profitz Pre-revealed"; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
+const GENERIC_DESCRIPTION = "... i hope it's rare"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://bafybeie6xpztcmgbj5diknd2mboahblwpksahqy3lvaestvzr7x2rtijzy.ipfs.nftstorage.link/"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -93,11 +159,11 @@ try {
 
 const solanaMetadata = {
   symbol: "YC",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  seller_fee_basis_points: 500, // Define how much % you want from secondary market sales 1000 = 10%
   external_url: "https://www.youtube.com/c/hashlipsnft",
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      address: "xC4f67c3d87E55467CDc055986c61d0E9d9778365",
       share: 100,
     },
   ],
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,22 +1,22 @@
 const contractAddress = "0x0776cB92D6D2b2aE372e0ea7FdD617525296CfcB"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_h1 = "Not For Profitz NFT";
+const welcome_h2 = "Connect to MetaMask or Coinbase Wallet to Get Started";
+const welcome_p = 'Welcome! You must be down bad like me. Its ok though, Im just here for community. Open-source project for future bLuEcHiP holders.';
+const h1_presale_coming_soon = "Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
+const p_presale_coming_soon = "Relax. You'll be rich soon. Stay tuned for updates!";
+const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. you must have fucked your community 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Greedy mfer! 🎉";
+const p_public_mint = "Mint if you love community but arent here for profitz! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Mint Your Not For Profitz NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_presale_already_minted = "Join The Community...";
+const button_public_mint = "Mint";
 const mint_failed = "Minting failed. 😢 Please try again.";
