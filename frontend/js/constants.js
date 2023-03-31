fcc7d084652b54b8530c7b021b5d89eee2591aa5
Down <not4profitnft@gmail.com>
2023-03-31 14:56:58 -0400

    welp

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -20,3 +20,5 @@ const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community...";
 const button_public_mint = "Mint";
 const mint_failed = "Minting failed. 😢 Please try again.";
+
+
