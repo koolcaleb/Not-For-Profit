29f6329030549992a6d5ce0758372fde995034eb
Down <not4profitnft@gmail.com>
2023-03-31 22:39:46 -0400

    c

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x2f4B1d73675107C9C82E5B9351ff9C59F0fa71e9"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Not For Profitz (NFT deez nutz)";
+const welcome_h1 = "Not For Profitz NFT";
 const welcome_h2 = "Connect MetaMask or Coinbase Wallet to Get Started";
-const welcome_p = 'Welcome! You must be down bad like me. Its ok though, Im just here for cOmMuNiTy.';
+const welcome_p = "Welcome! You must be down bad like me. Its ok though, we're just here for cOmMuNiTy.";
 const h1_presale_coming_soon = "Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Is Live Bitches!!";
 const h1_public_mint = "Public Minting Is Open Bitches!!";
